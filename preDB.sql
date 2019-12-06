228ab700d06b87eb90ac2d1f1d3589bd0696b138
tys1998 <tanyongshan1998@outlook.com>
2019-12-07 00:13:40 +0800

    解决命名冲突

--- preDB.sql
+++ preDB.sql
@@ -51,35 +51,35 @@ CREATE TABLE Supplier( -- 供应商
 );
 
 CREATE TABLE Drug( -- 药品
-	Dno VARCHAR(20) PRIMARY KEY, -- 编号
-	Dname VARCHAR(20) NOT NULL, -- 名称
-	Dlife SMALLINT NOT NULL -- 保质期 (天数)
+	PDno VARCHAR(20) PRIMARY KEY, -- 编号
+	PDname VARCHAR(20) NOT NULL, -- 名称
+	PDlife SMALLINT NOT NULL -- 保质期 (天数)
 );
 
 CREATE TABLE InventoryDrug( -- 库存药品
-	Dno VARCHAR(20), -- 编号
-	Dbatch DATE, -- 批次
-	Dnum SMALLINT NOT NULL, -- 数量
+	PDno VARCHAR(20), -- 编号
+	PDbatch DATE, -- 批次
+	PDnum SMALLINT NOT NULL, -- 数量
 	Sno VARCHAR(20) NOT NULL, -- 供应商编号
 	SAno VARCHAR(20) NOT NULL, -- 入库库存管理员编号
 	Stime DATETIME NOT NULL DEFAULT GETDATE(), -- 入库时间
-	PRIMARY KEY(Dno, Dbatch),
-	FOREIGN KEY (Dno) REFERENCES Drug(Dno),
+	PRIMARY KEY(PDno, PDbatch),
+	FOREIGN KEY (PDno) REFERENCES Drug(PDno),
 	FOREIGN KEY (Sno) REFERENCES Supplier(Sno),
 	FOREIGN KEY (SAno) REFERENCES Admin(Ano)
 );
 
 CREATE TABLE DestroyedDrug( -- 已销毁药品
-	Dno VARCHAR(20), -- 编号
-	Dbatch DATE, -- 批次
-	Dnum SMALLINT NOT NULL, -- 数量
+	PDno VARCHAR(20), -- 编号
+	PDbatch DATE, -- 批次
+	PDnum SMALLINT NOT NULL, -- 数量
 	Sno VARCHAR(20) NOT NULL, -- 供应商编号
 	SAno VARCHAR(20) NOT NULL, -- 入库库存管理员编号
 	Stime DATETIME NOT NULL, -- 入库时间
 	DAno VARCHAR(20) NOT NULL, -- 销毁库存管理员编号
 	Dtime DATETIME NOT NULL DEFAULT GETDATE(), -- 销毁时间
-	PRIMARY KEY(Dno, Dbatch),
-	FOREIGN KEY (Dno) REFERENCES Drug(Dno),
+	PRIMARY KEY(PDno, PDbatch),
+	FOREIGN KEY (PDno) REFERENCES Drug(PDno),
 	FOREIGN KEY (Sno) REFERENCES Supplier(Sno),
 	FOREIGN KEY (SAno) REFERENCES Admin(Ano),
 	FOREIGN KEy (DAno) REFERENCES Admin(Ano)
@@ -99,11 +99,11 @@ CREATE TABLE Prescription( -- 处方
 
 CREATE TABLE PID( -- 处方包含的药品
 	Pno INT, -- 处方编号
-	Dno VARCHAR(20), -- 药品编号
-	Dnum SMALLINT NOT NULL, -- 药品数量
-	PRIMARY KEY (Pno, Dno),
+	PDno VARCHAR(20), -- 药品编号
+	PDnum SMALLINT NOT NULL, -- 药品数量
+	PRIMARY KEY (Pno, PDno),
 	FOREIGN KEY (Pno) REFERENCES Prescription(Pno),
-	FOREIGN KEY (Dno) REFERENCES Drug(Dno)
+	FOREIGN KEY (PDno) REFERENCES Drug(PDno)
 );
 
 INSERT INTO Admin(Ano, Aname, Asex, Aage, Apwd) VALUES('admin1111', '李勇', 1, 25, 'admin1111');
@@ -143,64 +143,64 @@ INSERT INTO Supplier(Sno, Sname, Saddr, Sphone)
 INSERT INTO Supplier(Sno, Sname, Saddr, Sphone)
 	VALUES('supplier9999', '贵州省药材公司', '贵阳市富源北路22号A区', '17899999999');
 
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug1111', '阿莫西林胶囊', 180);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug2222', '氨苄西林钠', 120);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug3333', '青霉素钠', 90);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug4444', '磷霉素钠', 360);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug5555', '头孢氨苄甲氧苄啶胶囊', 270);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug6666', '头孢拉定胶囊', 120);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug7777', '头孢曲松钠', 90);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug8888', '头孢吡肟', 180);
-INSERT INTO Drug(Dno, Dname, Dlife) VALUES('drug9999', '头孢噻肟钠', 480);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug1111', '阿莫西林胶囊', 180);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug2222', '氨苄西林钠', 120);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug3333', '青霉素钠', 90);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug4444', '磷霉素钠', 360);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug5555', '头孢氨苄甲氧苄啶胶囊', 270);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug6666', '头孢拉定胶囊', 120);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug7777', '头孢曲松钠', 90);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug8888', '头孢吡肟', 180);
+INSERT INTO Drug(PDno, PDname, PDlife) VALUES('drug9999', '头孢噻肟钠', 480);
 
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug1111', '2019-06-20', 15, 'supplier1111', 'admin1111', '2019-06-25 10:30:30.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug2222', '2019-08-18', 32, 'supplier2222', 'admin1111', '2019-08-25 10:28:23.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug3333', '2019-09-30', 180, 'supplier3333', 'admin2222', '2019-10-10 15:05:26.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug4444', '2019-06-01', 280, 'supplier4444', 'admin2222', '2019-06-10 16:45:39.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug5555', '2019-03-15', 5, 'supplier5555', 'admin3333', '2019-03-25 09:16:30.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug6666', '2019-09-01', 50, 'supplier6666', 'admin3333', '2019-09-10 10:30:30.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug7777', '2019-10-08', 50, 'supplier7777', 'admin4444', '2019-10-11 10:36:55.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug8888', '2019-11-05', 245, 'supplier8888', 'admin4444', '2019-11-09 14:29:16.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug9999', '2019-11-21', 165, 'supplier9999', 'admin5555', '2019-11-25 11:53:55.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug1111', '2019-11-25', 270, 'supplier5555', 'admin2222', '2019-11-30 13:59:53.000');
-INSERT INTO InventoryDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime)
+INSERT INTO InventoryDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime)
 	VALUES('drug2222', '2019-11-27', 500, 'supplier4444', 'admin3333', '2019-11-30 15:25:22.000');
 
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug9999', '2018-06-10', 5, 'supplier9999', 'admin5555', '2018-06-25 10:30:30.000',
 														'admin1111', '2019-09-30 15:22:16.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug8888', '2018-08-18', 7, 'supplier8888', 'admin5555', '2018-08-25 10:28:23.000',
 														'admin2222', '2019-02-06 16:13:43.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug7777', '2018-09-30', 13, 'supplier7777', 'admin4444', '2018-10-10 15:05:26.000',
 														'admin1111', '2018-12-24 09:16:55.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug6666', '2018-06-01', 9, 'supplier6666', 'admin4444', '2018-06-10 16:45:39.000',
 														'admin4444', '2018-09-24 13:59:15.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug5555', '2018-03-15', 15, 'supplier5555', 'admin3333', '2018-03-25 09:16:30.000',
 														'admin1111', '2018-12-05 11:05:06.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug4444', '2018-06-01', 3, 'supplier4444', 'admin3333', '2018-06-10 10:30:30.000',
 														'admin1111', '2019-05-10 16:17:05.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug3333', '2018-10-08', 5, 'supplier3333', 'admin2222', '2018-10-11 10:36:55.000',
 														'admin5555', '2019-01-02 12:05:53.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug2222', '2018-11-05', 7, 'supplier2222', 'admin2222', '2018-11-09 14:29:16.000',
 														'admin3333', '2019-02-26 10:59:05.000');
-INSERT INTO DestroyedDrug(Dno, Dbatch, Dnum, Sno, SAno, Stime, DAno, Dtime)
+INSERT INTO DestroyedDrug(PDno, PDbatch, PDnum, Sno, SAno, Stime, DAno, Dtime)
 	VALUES('drug1111', '2018-11-21', 17, 'supplier1111', 'admin1111', '2018-11-25 11:53:55.000',
 														'admin2222', '2019-05-05 15:15:29.000');
 
@@ -223,22 +223,22 @@ INSERT INTO Prescription(Pid, Dno, Ptime, Nno, Htime, Pstate)
 INSERT INTO Prescription(Pid, Dno, Ptime, Nno, Htime, Pstate)
 	VALUES('44019921016', 'doctor1111', '2019-12-01 15:35:16.000', NULL, NULL, 0);
 
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(1, 'drug1111', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(1, 'drug3333', 1);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(2, 'drug5555', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(3, 'drug7777', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(3, 'drug8888', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(3, 'drug9999', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(4, 'drug2222', 5);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(4, 'drug3333', 4);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(5, 'drug2222', 4);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(5, 'drug3333', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(5, 'drug5555', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(5, 'drug7777', 7);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(5, 'drug9999', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(6, 'drug2222', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(6, 'drug4444', 1);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(8, 'drug6666', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(8, 'drug8888', 3);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(8, 'drug9999', 2);
-INSERT INTO PID(Pno, Dno, Dnum) VALUES(9, 'drug1111', 1);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(1, 'drug1111', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(1, 'drug3333', 1);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(2, 'drug5555', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(3, 'drug7777', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(3, 'drug8888', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(3, 'drug9999', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(4, 'drug2222', 5);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(4, 'drug3333', 4);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(5, 'drug2222', 4);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(5, 'drug3333', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(5, 'drug5555', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(5, 'drug7777', 7);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(5, 'drug9999', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(6, 'drug2222', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(6, 'drug4444', 1);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(8, 'drug6666', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(8, 'drug8888', 3);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(8, 'drug9999', 2);
+INSERT INTO PID(Pno, PDno, PDnum) VALUES(9, 'drug1111', 1);
